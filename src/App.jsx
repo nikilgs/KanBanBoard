642d4dccafb63d74a672300df57e7cc1381c7b99
nikilgs <nikilgs143@gmail.com>
2025-09-26 15:22:55 +0530

    recent update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@
 import './App.css'
 import Board from './components/Board'
 import { BoardProvider } from './components/BoardContext'
-import Todo from './Todo'
 
 function App() {
 
   return (
     <div>
-      {/* <Todo /> */}
   <BoardProvider>
     <Board/>
 
